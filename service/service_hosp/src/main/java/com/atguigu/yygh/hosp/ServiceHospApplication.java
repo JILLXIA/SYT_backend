2b0cfcfd887cd8128da51159c94c664222f5bf9f
JILLXIA <171250011@smail.nju.edu.cn>
2022-04-02 17:18:32 +0800

    20220402 实现admin接口

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -2,8 +2,10 @@ package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.atguigu") // 确定扫描规则
 public class ServiceHospApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHospApplication.class, args);
